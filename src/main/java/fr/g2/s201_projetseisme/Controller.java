3eddeef36296926eb1f8eed45ab2987b0be5d40c
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2023-06-06 21:51:43 +0200

    Menu design avec animations

--- src/main/java/fr/g2/s201_projetseisme/Controller.java
+++ src/main/java/fr/g2/s201_projetseisme/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller {
+
+    @FXML
+    private HBox accueilHbox;
+    @FXML
+    private HBox statistiquesHbox;
+    @FXML
+    private HBox mapHbox;
+    @FXML
+    private Button accueilButton;
+    @FXML
+    private Button statistiquesButton;
+    @FXML
+    private Button mapButton;
 
     @FXML
     public void mapButtonClicked() throws IOException {
@@ -34,5 +48,41 @@ public class Controller {
         MainApp.scene = new Scene(fxmlLoader.load(), 800, 600);
         MainApp.stage.setScene(MainApp.scene);
         MainApp.stage.show();
+    }
+
+    @FXML
+    public void mouseEnterAccueil() {
+        accueilHbox.setStyle("-fx-background-color: #A2F5FF; -fx-border-color: grey;");
+        accueilButton.setStyle("-fx-background-color: #A2F5FF");
+    }
+
+    @FXML
+    public void mouseExitAccueil() {
+        accueilHbox.setStyle("-fx-background-color: #34EAFF; -fx-border-color: grey;");
+        accueilButton.setStyle("-fx-background-color: #34EAFF");
+    }
+
+    @FXML
+    public void mouseEnterStatistiques() {
+        statistiquesHbox.setStyle("-fx-background-color: #A2F5FF; -fx-border-color: grey;");
+        statistiquesButton.setStyle("-fx-background-color: #A2F5FF");
+    }
+
+    @FXML
+    public void mouseExitStatistiques() {
+        statistiquesHbox.setStyle("-fx-background-color: #34EAFF; -fx-border-color: grey;");
+        statistiquesButton.setStyle("-fx-background-color: #34EAFF");
+    }
+
+    @FXML
+    public void mouseEnterMap() {
+        mapHbox.setStyle("-fx-background-color: #A2F5FF; -fx-border-color: grey;");
+        mapButton.setStyle("-fx-background-color: #A2F5FF");
+    }
+
+    @FXML
+    public void mouseExitMap() {
+        mapHbox.setStyle("-fx-background-color: #34EAFF; -fx-border-color: grey;");
+        mapButton.setStyle("-fx-background-color: #34EAFF");
     }
 }
