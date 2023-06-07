d519117fc1a9a7e4cd6f6c8e6075eb9a6bb90f76
killbib-coder <killian.ferrier@etu.univ-amu.fr>
2023-06-07 11:45:43 +0200

    Ajout de la fonction pour sélectionner un fichier et le lire

--- src/main/java/fr/g2/s201_projetseisme/AccueilController.java
+++ src/main/java/fr/g2/s201_projetseisme/AccueilController.java
@@ -6,16 +6,42 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AccueilController extends Controller implements Initializable {
 
+    @FXML
+    Button importButton;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
+    public void chooseFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Sélectionner un fichier");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Fichiers CSV", "*.csv"));
+        Stage stage = (Stage) MainApp.stage;
+        java.io.File selectedFile = fileChooser.showOpenDialog(stage);
+
+        if (selectedFile != null) {
+            System.out.println("Fichier sélectionné : " + selectedFile.getAbsolutePath());
+            try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                System.out.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+            }
+        } else {
+            System.out.println("Aucun fichier sélectionné.");
+        }
     }
 }
--- src/main/java/fr/g2/s201_projetseisme/MainApp.java
+++ src/main/java/fr/g2/s201_projetseisme/MainApp.java
@@ -19,7 +19,7 @@ public class MainApp extends Application {
         scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("KAMA Séisme App");
         stage.setScene(scene);
-        this.stage = stage;
+        MainApp.stage = stage;
         stage.show();
     }
 
