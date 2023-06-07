19691996751d40923826dc1c12ac064a940b9441
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2023-06-07 19:05:55 +0200

    Statistiques Design + Graphiques contenant des valeurs exemples

--- src/main/java/fr/g2/s201_projetseisme/StatistiquesController.java
+++ src/main/java/fr/g2/s201_projetseisme/StatistiquesController.java
@@ -1,12 +1,12 @@
 package fr.g2.s201_projetseisme;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -17,15 +17,63 @@ import java.util.ResourceBundle;
 
 public class StatistiquesController extends Controller implements Initializable {
 
+    //barChart
     @FXML
-    BarChart barChart;
+    private BarChart<String, Float> barChart;
     @FXML
-    PieChart pieChart;
+    private NumberAxis barChartYAxis;
     @FXML
-    LineChart lineChart;
+    private CategoryAxis barChartXAxis;
+    private XYChart.Series<String, Float> barChartData;
+
+    //pieChart
+    @FXML
+    private PieChart pieChart;
+    private ObservableList<PieChart.Data> pieChartData;
+
+    //lineChart
+    @FXML
+    private LineChart lineChart;
+    @FXML
+    private NumberAxis lineChartYAxis;
+    @FXML
+    private CategoryAxis lineChartXAxis;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        //Exemple de remplissage du barChart
+        barChartData = new XYChart.Series<>();
+        barChartData.getData().add(new XYChart.Data("Ville 1",10));
+        barChartData.getData().add(new XYChart.Data("Ville 2",10));
+        barChartData.getData().add(new XYChart.Data("Ville 3",10));
+        barChart.getData().add(barChartData);
+
+        //Exemple de remplissage du pieChart
+        pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("2000", 10),
+                new PieChart.Data("2001", 10),
+                new PieChart.Data("2002", 10)
+        );
+        pieChart.setData(pieChartData);
+
+        //Exemple de remplissage du lineChart
+        XYChart.Series<String, Float> lineChartData1 = new XYChart.Series<>();
+        XYChart.Series<String, Float> lineChartData2 = new XYChart.Series<>();
+        XYChart.Series<String, Float> lineChartData3 = new XYChart.Series<>();
+        lineChartData1.getData().add(new XYChart.Data("2000",10));
+        lineChartData1.getData().add(new XYChart.Data("2001",20));
+        lineChartData1.getData().add(new XYChart.Data("2002",10));
+        lineChartData2.getData().add(new XYChart.Data("2000",20));
+        lineChartData2.getData().add(new XYChart.Data("2001",10));
+        lineChartData2.getData().add(new XYChart.Data("2002",20));
+        lineChartData3.getData().add(new XYChart.Data("2000",5));
+        lineChartData3.getData().add(new XYChart.Data("2001",10));
+        lineChartData3.getData().add(new XYChart.Data("2002",15));
+        lineChartData1.setName("Ville 1");
+        lineChartData2.setName("Ville 2");
+        lineChartData3.setName("Ville 3");
+        lineChart.getData().addAll(lineChartData1, lineChartData2, lineChartData3);
     }
 }
