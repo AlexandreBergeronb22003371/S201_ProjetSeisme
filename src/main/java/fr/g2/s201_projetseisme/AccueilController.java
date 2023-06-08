3bde25ad3a8155d9ecd55b65371058d56cc666d8
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2023-06-08 11:28:16 +0200

    pathLabel sauvegardé si changement de page + public static data qui contient les données du csv organisées correctement

--- src/main/java/fr/g2/s201_projetseisme/AccueilController.java
+++ src/main/java/fr/g2/s201_projetseisme/AccueilController.java
@@ -13,42 +13,66 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AccueilController extends Controller implements Initializable {
 
     @FXML
-    Button importButton;
+    private Button importButton;
 
     @FXML
-    Label pathLabel;
+    private Label pathLabel;
+
+    public static ArrayList<ArrayList<String>> data;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        data = new ArrayList<ArrayList<String>>();
+        if (pathString != "aucun") {
+            pathLabel.setText(pathString);
+            pathLabel.setStyle("-fx-text-fill: green");
+        }
+        else {
+            pathString = "aucun";
+            pathLabel.setText(pathString);
+            pathLabel.setStyle("-fx-text-fill: red");
+        }
     }
 
     public void chooseFile() {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Sélectionner un fichier");
+        fileChooser.setTitle("Sélectionnez un fichier");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Fichiers CSV", "*.csv"));
         Stage stage = (Stage) MainApp.stage;
         java.io.File selectedFile = fileChooser.showOpenDialog(stage);
 
         if (selectedFile != null) {
-            System.out.println("Fichier sélectionné : " + selectedFile.getAbsolutePath());
-            pathLabel.setText(" " + selectedFile.getAbsolutePath());
+            pathString = selectedFile.getAbsolutePath();
+            System.out.println("Fichier sélectionné : " + pathString);
+            pathLabel.setText(pathString);
             pathLabel.setStyle("-fx-text-fill: green");
             try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
                 String line;
+                String[] tempArr;
+                String delimiter = ",";
+                ArrayList<String> insideArr = new ArrayList<String>();
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    tempArr = line.split(delimiter);
+                    for (String tempStr : tempArr) {
+                        insideArr.add(tempStr);
+                    }
+                    data.add(insideArr);
                 }
             } catch (IOException e) {
                 System.out.println("Erreur lors de la lecture du fichier : " + e.getMessage());
             }
         } else {
+            pathString = "aucun";
             System.out.println("Aucun fichier sélectionné.");
-            pathLabel.setText(" aucun");
+            pathLabel.setText(pathString);
             pathLabel.setStyle("-fx-text-fill: red");
         }
     }
