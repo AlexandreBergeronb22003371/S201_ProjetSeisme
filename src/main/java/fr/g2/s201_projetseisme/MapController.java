21771178b9d3969eade173282ca8063234c715bb
AlexandreBergeronb22003371 <alexandrebergeron12@gmail.com>
2023-06-11 20:58:15 +0200

    Implémentation du code de Mdallah pour la Map + lien de la map avec les données du csv lues

--- src/main/java/fr/g2/s201_projetseisme/MapController.java
+++ src/main/java/fr/g2/s201_projetseisme/MapController.java
@@ -3,19 +3,82 @@ package fr.g2.s201_projetseisme;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import com.gluonhq.maps.MapPoint;
+import com.gluonhq.maps.MapLayer;
+import com.gluonhq.maps.MapView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+
 public class MapController extends Controller implements Initializable {
+
+    @FXML
+    public MapView mapView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        MapPoint mapPoint = new MapPoint(46.227638, 2.213749);
 
+        /* Zoom de 5 */
+        mapView.setZoom(3);
+
+        /* Centre la carte sur le point */
+        mapView.flyTo(0, mapPoint, 0.1);
+
+        if (DataSorter.data != null) {
+            if (!(DataSorter.data.isEmpty())) {
+                mapPointTrace();
+            }
+        }
+    }
+
+    public class CustomCircleMarkerLayer extends MapLayer {
+
+        private final MapPoint mapPoint;
+        private final Circle circle;
+
+        public CustomCircleMarkerLayer(MapPoint mapPoint) {
+            this.mapPoint = mapPoint;
+            this.circle = new Circle(5, Color.RED);
+            this.getChildren().add(circle);
+        }
+
+        @Override
+        protected void layoutLayer() {
+            Point2D point2d = getMapPoint(mapPoint.getLatitude(), mapPoint.getLongitude());
+            circle.setTranslateX(point2d.getX());
+            circle.setTranslateY(point2d.getY());
+        }
+    }
+
+    public void mapPointTrace(){
+
+        for (int i = 1 ; i < DataSorter.data.size() ; ++i) {
+
+            String latitudeStr = DataSorter.data.get(i).get(DataSorter.latitudeColumnIndex);
+            String longitudeStr = DataSorter.data.get(i).get(DataSorter.longitudeColumnIndex);
+
+            if (!(latitudeStr.isEmpty() || longitudeStr.isEmpty())) {
+                double latitude = Double.parseDouble(DataSorter.data.get(i).get(DataSorter.latitudeColumnIndex));
+                double longitude = Double.parseDouble(DataSorter.data.get(i).get(DataSorter.longitudeColumnIndex));
+
+                MapPoint mapPoint = new MapPoint(latitude, longitude);
+                MapLayer mapLayer = new CustomCircleMarkerLayer(mapPoint);
+                mapView.addLayer(mapLayer);
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.g2.s201_projetseisme {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.gluonhq.maps;
 
 
     opens fr.g2.s201_projetseisme to javafx.fxml;
